fcbdb987b3eea0781b3f503b3639d14adcc78764
brayanmora0992 <brayanmora0992@gmail.com>
2023-07-18 12:33:34 -0400

    fix pylint

--- service/routes.py
+++ service/routes.py
@@ -61,7 +61,6 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -85,7 +84,7 @@ def get_accounts(account_id):
     """
     Reads an Account
     This endpoint will read an Account based the account_id that is requested
-    """    
+    """
     app.logger.info("Request to read an Account with id: %s", account_id)
     
     account = Account.find(account_id)
@@ -93,7 +92,6 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
-
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -114,7 +112,6 @@ def update_accounts(account_id):
     account.update()
 
     return account.serialize(), status.HTTP_200_OK
-    
 
 ######################################################################
 # DELETE AN ACCOUNT
